0723eb1bc3a51415cd990f2ee3b93c13e34e5810
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-10-02 02:09:42 +0000

    corrige le fichier de film

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,19 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/index" })
   end
 
+  def create 
+    m = Movie.new
+    m.title = params.fetch("title_field")
+    m.year = params.fetch("year_field")
+    m.duration = params.fetch("duration_field")
+    m.description = params.fetch("description_field")
+    m.image = params.fetch("image_field")
+    m.director_id = params.fetch("director_field")
+
+    m.save
+    redirect_to("/movies")
+  end 
+
   def show
     the_id = params.fetch("path_id")
 
